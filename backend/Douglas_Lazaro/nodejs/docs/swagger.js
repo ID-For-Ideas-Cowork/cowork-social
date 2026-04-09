e5f0bda5e002b83380b2d89bbeca9a37555a0485
Douglas Lázaro <lazarodou@gmail.com>
2026-04-09 15:49:15 -0600

    Actualizacion de servidor de produccion en swagger

--- backend/Douglas_Lazaro/nodejs/docs/swagger.js
+++ backend/Douglas_Lazaro/nodejs/docs/swagger.js
@@ -11,12 +11,12 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 5000}`,
-        description: 'Development server',
-      },
-      {
         url: 'https://redes-sociales-8eql.onrender.com',
         description: 'Production server',
+      },
+      {
+        url: `http://localhost:${process.env.PORT || 5000}`,
+        description: 'Development server',
       }
     ],
   },
